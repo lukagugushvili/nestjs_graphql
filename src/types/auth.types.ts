4ff19be52f42602ec6b5b38c705e9f994e68283b
lukagugushvili <gugushvili58@gmail.com>
2024-10-22 06:01:38 -0700

    add validation on SignUpTypes and SignUpTypes

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,10 +1,14 @@
+import { IsNotEmpty } from 'class-validator';
 import { SignUpDto } from 'src/auth/dto/sign-up.dto';
 
 export class SignUpTypes {
+  @IsNotEmpty()
   message: string;
+
   user: SignUpDto;
 }
 
 export class SignInTypes {
+  @IsNotEmpty()
   access_token: string;
 }
